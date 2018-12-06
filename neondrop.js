4bbcc129cea68134dc7f55c5ae476d7ade2fc6da
syasin-5d <syasinahya@gmail.com>
2018-12-06 14:40:12 +0900

    first commit

---
+++ neondrop.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const yellow = "#FFFF00";
+const red = "#FD1C03";
+const green = "#33FF00";
+const blue = "#0062FF";
+const cyan = "#00FFFF";
+const pink = "#CC00FF";
+
+function draw(){
+    var canvas = document.getElementById("canvas");
+    if(canvas.getContext){
+        var ctx = canvas.getContext("2d");
+
+        var colors = [yellow, blue, cyan, pink];
+        colors = colors.concat([red, green]);
+        // 寒色系を多めにしてるっす
+        var numOfTriangles = 150;
+        for(var i = 0; i < numOfTriangles; ++i){
+            var x = Math.floor(Math.random()*canvas.width);
+            var y = Math.floor(Math.random()*canvas.height);
+            var color = colors[Math.floor(Math.random() * colors.length)];
+            var rotate = Math.floor(Math.random()*4)*90;
+            drawTriangle({ctx:ctx, x:x, y:y, rotate:rotate, color:color});
+        }
+
+    }
+}
+
+function drawTriangle(parameters){
+    const ctx = parameters.ctx;
+    if (ctx == null) {
+        throw new Error();
+    }
+    const x = parameters.x;
+    const y = parameters.y;
+    const width = parameters.width || 25;
+    const height = parameters.height || 25;
+    const rotate = parameters.rotate || 0;
+    const lineWidth = parameters.lineWidth || 2;
+    const color = parameters.color || "#000000";
+
+    draw(ctx,x,y,width,height,rotate,lineWidth,color);
+
+    function draw(ctx,x,y,width,height,rotate,lineWidth,color){
+        ctx.save();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.translate(parseInt(canvas.width/2), parseInt(canvas.height/2));
+        ctx.rotate(rotate/180*Math.PI);
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+        ctx.lineTo(x+width,y+width);
+        ctx.lineTo(x+width,y-width);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.restore();
+    }
+}
---
+++ neondrop.md
@@ -0,0 +1,36 @@
+# Neon Drop っぽいWebサイト
+## Neon Drop っぽいってなんだ？
+> ポップ系ブランド。ネオンカラーを多用したスポーティな衣装が特徴。
+（Wikipedia)
+### ネオンカラー
+[Neon Sign Color Chart](https://www.custom-neon-signs.com/Colors)
+[Color Code: NEON](http://www.fashiontrendsetter.com/content/color_trends/color-decoder/Color-Code-Neon.html)
+#### Neon Yellow
+- #FFFF00
+- #FFFF33
+- #F2EA02
+- #E6FB04
+
+#### Neon Red
+- #FF0000
+- #FD1C03
+- #FF3300
+- #FF6600
+
+#### Neon Green
+- #00FF00
+- #00FF33
+- #00FF66
+- #33FF00
+
+#### Neon Blue
+- #00FFFF
+- #099FFF
+- #0062FF
+- #0033FF
+
+#### Neon Pink
+- #FF00FF
+- #FF00CC
+- #FF0099
+- #CC00FF
